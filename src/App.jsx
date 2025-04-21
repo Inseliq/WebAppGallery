0cb77adc78be271ad8e8c5a18218c4b2e8044291
Nikita KoSH <150700636+Inseliq@users.noreply.github.com>
2025-04-21 10:16:00 +0300

    upd --f

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>new Code runers</code> and save to test HTML
         </p>
       </div>
       <p className="read-the-docs">
